5bc93051ce4e4badff90058191ba3b7284cfdaa1
Emmy Ni <emmyni220@gmail.com>
2023-02-16 03:10:04 -0500

    UI changes

--- Assets/SunMovement.cs
+++ Assets/SunMovement.cs
@@ -6,25 +6,32 @@ using UnityEngine.Networking;
 public class SunMovement : Movement
 {
     public override void OnCollisionEnter(Collision col)
-    {
+    {   
+        new_x = Random.Range(originalTarget.x, originalTarget.x + 0.5f);
+        new_y = Random.Range(1.35f, 1.5f);
+
         currTarget = new Vector3(
-            Random.Range(originalTarget.x - 0.20f, originalTarget.x + 0.20f), 
-            Random.Range(originalTarget.y + 0.20f, originalTarget.y + 1.75f), 
+            new_x, 
+            new_y, 
             originalTarget.z
         );
 
         gameObject.transform.position = new Vector3(
-            Random.Range(-2.0f, 2.0f), 
-            Random.Range(2.0f, 4.0f), 
-            15.0f
+            new_x, 
+            new_y, 
+            10f
         );
 
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        
         if (col.gameObject.name == "Sword") {
             ScoreManager.scoreManagerInstance.AddBonus();
+            sleeve.ActionOne();
         }
 
         if (col.gameObject.name == "Shield") {
-            StartCoroutine(GetRequest("http://192.168.206.205:80/1/on"));
+            // StartCoroutine(GetRequest("http://192.168.206.205:80/1/on"));
+            // ScoreManager.scoreManagerInstance.ResetCombo();
         }
 
         StartCoroutine( WaitHandler() );
